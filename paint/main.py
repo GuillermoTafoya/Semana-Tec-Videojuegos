ae42b6c49506b3b87e288981284ddbcf893b7974
Guillermo Tafoya <A01633790@tec.mx>
2022-03-22 12:20:00 -0600

    Added Colors Yellow and Purple

--- paint/main.py
+++ paint/main.py
@@ -73,12 +73,18 @@ state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
+### THE COMMANDS ARE CASE SENSITIVE ###
+# Undo
 onkey(undo, 'u')
+# Color changes
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
+onkey(lambda: color('purple'), 'P')
+# Shapes
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
